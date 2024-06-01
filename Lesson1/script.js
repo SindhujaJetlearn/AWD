2abe0b8bf7213b01b3a661bfe0f6f91be5876a43
SindhujaJetlearn <158983007+SindhujaJetlearn@users.noreply.github.com>
2024-06-01 19:30:10 +0530

    Add files via upload

---
+++ Lesson1/script.js
@@ -0,0 +1,10 @@
+// 'screen' is name  of a function
+window.onresize = screen;
+window.onload = screen;
+
+// Function named 'screen' 
+
+function screen() {
+  Width = window.innerWidth;
+  document.getElementById("size").innerHTML = "Width : " + Width + " px" 
+}
